cfd18c3b852921b2396a3c5a0d4f21ebfeca2efa
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-29 22:56:27 -0300

    create controller Estudios

---
+++ M3S9-jogos.webApi/Controllers/EstudiosController.cs
@@ -0,0 +1,73 @@
+﻿using M3S9_jogos.webApi.Domain;
+using M3S9_jogos.webApi.Repositores;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M3S9_jogos.webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiosController : ControllerBase
+    {
+        readonly EstudioRepository _estudioRepository;
+
+        public EstudiosController(EstudioRepository estudioRepository)
+        {
+            _estudioRepository = estudioRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Estudio>> Get()
+        {
+            List<Estudio> estudios = _estudioRepository.Get();
+            if (estudios == null || estudios.Count == 0)
+            {
+                return NoContent();
+            }
+            return Ok(estudios);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Estudio> Get(int id)
+        {
+            Estudio estudio = _estudioRepository.Get(id);
+            if (estudio == null)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Estudio> Post(Estudio estudio)
+        {
+            _estudioRepository.Insert(estudio);
+
+            return Ok(estudio);
+
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Estudio estudio)
+        {
+            if (_estudioRepository.Get(id) == null)
+                return NotFound();
+            if (estudio.Id != id)
+                return BadRequest();
+
+            _estudioRepository.Update(estudio);
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var estudio = _estudioRepository.Get(id);
+                if (estudio== null)
+                return NotFound();
+
+            _estudioRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
--- M3S9-jogos.webApi/Program.cs
+++ M3S9-jogos.webApi/Program.cs
@@ -1,8 +1,14 @@
+using M3S9_jogos.webApi.Infra;
+using M3S9_jogos.webApi.Repositores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<JogoDbContext>();
+builder.Services.AddScoped<EstudioRepository>();
+
 
 var app = builder.Build();
 
