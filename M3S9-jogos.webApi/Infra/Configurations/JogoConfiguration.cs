5edbe00a838a9b584b69137722460e7d07dab8a9
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-02 07:53:47 -0300

    Jogorepository create

--- M3S9-jogos.webApi/Infra/Configurations/JogoConfiguration.cs
+++ M3S9-jogos.webApi/Infra/Configurations/JogoConfiguration.cs
@@ -8,9 +8,11 @@ namespace M3S9_jogos.webApi.Infra.Configurations
     {
         public void Configure(EntityTypeBuilder<Jogo> builder)
         {
-            builder.ToTable("Jogosos");
+            builder.ToTable("Jogos");
             builder.HasKey(x => x.Id);
             builder.Property(p => p.Id).UseIdentityColumn();
+            
+            // mapeamento do relacionamento
             builder.HasOne(P => P.Estudio)
                 .WithMany(P => P.Jogos)
                 .HasForeignKey(p => p.EstudioId);
